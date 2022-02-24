952e1dc7ea0031b86f828fc704628301ea70d2d0
Ervin <ervinsv92@gmail.com>
2022-02-23 18:59:09 -0600

    sdf

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ let app = express();
 app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-let PORT = 8080;
+let PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res, next)=>{
-    res.json({ok:'ok'})
-})
+app.use(express.static('public'));
 
 app.listen(PORT, err =>{
     if(err) throw new Error(`Error en servidor ${err}`)
